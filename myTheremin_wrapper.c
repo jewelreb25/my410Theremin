4764337b3f03837268267d61d4704e7ac790e8af
jewelreb25 <157763100+jewelreb25@users.noreply.github.com>
2025-12-10 20:17:05 -0500

    Add files via upload
    turn the buzzer off if the hand isn't above the sensor

--- myTheremin_wrapper.c
+++ myTheremin_wrapper.c
@@ -2,39 +2,59 @@ extern int myTheremin(void);
 extern int poll_ADC(void);
 extern int updateFreq(int);
 
+int smooth_adc(void);
+
 int main(){
   myTheremin();
   while(1){
-  long int valueRead= poll_ADC();
-      if(valueRead>0&& valueRead<600){
+      int valueRead=smooth_adc();
+      if(valueRead>0&& valueRead<450){
           updateFreq(318);
       }
-      else if(valueRead>600&& valueRead<1200){
+      else if(valueRead>451&& valueRead<800){
                 updateFreq(357);
       }
-      else if(valueRead>1200&& valueRead<1800){
+      else if(valueRead>801&& valueRead<1150){
                 updateFreq(379);
       }
-      else if(valueRead>1800&& valueRead<2400){
+      else if(valueRead>1151&& valueRead<1500){
                 updateFreq(425);
       }
-      else if(valueRead>2400&& valueRead<3000){
+      else if(valueRead>1501&& valueRead<1850){
                 updateFreq(477);
       }
-      else if(valueRead>3000&& valueRead<3600){
+      else if(valueRead>1851&& valueRead<2200){
                 updateFreq(506);
       }
-      else if(valueRead>3600&& valueRead<4095){
+      else if(valueRead>2201&& valueRead<2250){
                 updateFreq(567);
       }
-      else if(valueRead>4200&& valueRead<4500){
+      else if(valueRead>2251&& valueRead<2700){
                 updateFreq(637);
       }
-      else if(valueRead>2000&& valueRead<3000){
+      else if(valueRead>2701&& valueRead<2900){
                 updateFreq(715);
       }
+      else if(valueRead>2901&& valueRead<3000){
+                updateFreq(758);
+      }
+      else if(valueRead>3000&& valueRead<3800){
+                updateFreq(850);
+      }
+      else if(valueRead>3800&& valueRead<3810){
+                updateFreq(955);
+      }
       else{
-          updateFreq(955);
+          updateFreq(0);
       }
   }
+}
+
+int smooth_adc(void){
+   int sum = 0;
+   int i;
+   for(i = 0; i < 16; i++){
+     sum += poll_ADC();
+   }
+   return (sum/16);
 }
